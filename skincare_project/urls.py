9ac3631f7cab980acdb792fe9ff8f281281b4492
RJChoe <120413939+RJChoe@users.noreply.github.com>
2025-10-29 21:15:32 -0600

    urls.py & views.py updated homepage to home

--- skincare_project/urls.py
+++ skincare_project/urls.py
@@ -20,7 +20,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage),
+    path('', views.home),
     path('user', views.user),
     path('product', views.product),
 ]
